90dde77ea2f3aa4152f9a745bdaf19123814fe83
KoreZhong252 <77712486+zhonk252@users.noreply.github.com>
2021-03-25 22:38:22 +0800

    Final

---
+++ script.js
@@ -0,0 +1,10 @@
+unction setup() {
+    createCanvas(windowWidth, windowHeight);}
+
+function draw(){
+
+    background(182, 221, 255);
+    noStroke();
+    fill(0);
+    ellipse(mouseX,mouseY,20,20);
+}
